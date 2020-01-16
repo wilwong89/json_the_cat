3784b30df0b3f5016100fabadbe3c904707daef7
Wilson Wong <wilwong89@gmail.com>
2020-01-16 23:21:56 +0000

    update and added tests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,15 @@
 const request = require('request');
-const args = [...process.argv.slice(2)];
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${args[0]}`, (error, response, body) => {
-  if (error) {
-    console.log("Error:",error);
-  } else {
-    let data = JSON.parse(body);
-  
-    if (data && data.length) console.log(data[0].description);
-    else {
-      console.log("No match found");
+const fetchBreedDescription = (breedName, callback) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      // Nothing
+    } else {
+      body = JSON.parse(body);
     }
-  }
-});
+    
+    callback(error, body);
+  });
+};
+
+module.exports = {fetchBreedDescription};
